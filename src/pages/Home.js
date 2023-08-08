cf5c214d85daf6d75fed40f4afdb8b30f764aaa6
Brenda Cayzac <brendacayzac@gmail.com>
2023-08-08 10:22:08 +0200

    added some styling and content to home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,19 @@
 import "../App.scss";
+import "../assets/styles/HomePage.scss";
 
 export const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="page home">
+      <figure>
+        <img
+          src={require("../assets/img/pf_logo.png")}
+          alt="Pathfinder 2nd edition logo"
+        />
+      </figure>
+      <p>
+        This website is a compilation of information for the Pathfinder
+        Community.
+      </p>
+    </div>
+  );
 };
