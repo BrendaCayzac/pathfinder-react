cd2495e6cb8023c3bd34965eb031c8cdf23894b8
Brenda Cayzac <brendacayzac@gmail.com>
2023-08-08 11:20:58 +0200

    added remaining ancestries to js object

--- src/middleware/AncestriesObject.js
+++ src/middleware/AncestriesObject.js
@@ -1,5 +1,32 @@
 export const ancestries = [
   {
+    img: "Anadi.png",
+    ancestry: "anadi",
+    book: "The Mwangi Expanse",
+    description:
+      "Anadi people are reclusive, sapient spiders who hail from the jungles of southern Garund.",
+    abilityBoosts: "Dexterity, Wisdom, free",
+    abilityFlaw: "Free",
+  },
+  {
+    img: "Android.png",
+    ancestry: "android",
+    book: "Ancestry Guide",
+    description:
+      "Technological wonders from another world, androids have synthetic bodies and living souls.",
+    abilityBoosts: "Dexterity, Intelligence, free",
+    abilityFlaw: "Charisma",
+  },
+  {
+    img: "Automaton.png",
+    ancestry: "automaton",
+    book: "Guns & Gears",
+    description:
+      "These intelligent constructs house actual souls and represent what remains of a dying empire's last attempt at greatness.",
+    abilityBoosts: "Strength, free",
+    abilityFlaw: "—",
+  },
+  {
     img: "Azarketi.png",
     ancestry: "azarketi",
     book: "Absalom, City of Lost Omens",
@@ -16,6 +43,15 @@ export const ancestries = [
       "Curious and gregarious wanderers, catfolk combine features of felines and humanoids in both appearance and temperament.",
     abilityBoosts: "Dexterity, Charisma, free",
     abilityFlaw: "Wisdom",
+  },
+  {
+    img: "Conrasu.png",
+    ancestry: "conrasu",
+    book: "The Mwangi Expanse",
+    description:
+      "Conrasus are shards of cosmic force given consciousness who construct intricate exoskeletons to interface with the mortal world.",
+    abilityBoosts: "Constitution, Wisdom, free",
+    abilityFlaw: "Charisma",
   },
   {
     img: "Dwarf.png",
@@ -46,6 +82,24 @@ export const ancestries = [
     abilityFlaw: "—",
   },
   {
+    img: "Fleshwarp.png",
+    ancestry: "fleshwarp",
+    book: "Ancestry Guide",
+    description:
+      "Fleshwarps are people whose forms were created or radically transformed by magic, alchemy, or unnatural energies.",
+    abilityBoosts: "Constitution, free",
+    abilityFlaw: "—",
+  },
+  {
+    img: "Ghoran.png",
+    ancestry: "ghoran",
+    book: "Impossible Lands",
+    description:
+      "These intelligent plant people, created by a long-dead druid, possess" +
+      " a sort of immortality through their seeds—unless these are destroyed by external events other than merely the ravages of time.",
+    abilityBoosts: "Constitution, free",
+  },
+  {
     img: "Gnoll.png",
     ancestry: "gnoll",
     book: "The Mwangi Expanse",
@@ -73,6 +127,26 @@ export const ancestries = [
     abilityFlaw: "Wisdom",
   },
   {
+    img: "Goloma.png",
+    ancestry: "goloma",
+    book: "The Mwangi Expanse",
+    description:
+      "Golomas fear most other people and deliberately use their unusual" +
+      " biology to frighten off those they consider to be dangerous predators.",
+    abilityBoosts: "Wisdom, free",
+    abilityFlaw: "—",
+  },
+  {
+    img: "Grippli.png",
+    ancestry: "grippli",
+    book: "The Mwangi Expanse",
+    description:
+      "Gripplis are a shy and cautious people who generally seek to avoid" +
+      " being drawn into the complicated and dangerous affairs of others.",
+    abilityBoosts: "Dexterity, Wisdom, free",
+    abilityFlaw: "Strength",
+  },
+  {
     img: "Halfling.png",
     ancestry: "halfling",
     book: "Core Rulebook",
@@ -82,12 +156,174 @@ export const ancestries = [
     abilityFlaw: "Strength",
   },
   {
+    img: "Hobgoblin.png",
+    ancestry: "hobgoblin",
+    book: "Character Guide",
+    description:
+      "Taller and stronger than their goblin kin, hobgoblins are equals in strength and size to humans, with broad shoulders and long, powerful arms.",
+    abilityBoosts: "Constitution, Intelligence, free",
+    abilityFlaw: "Wisdom",
+  },
+  {
     img: "Human.png",
     ancestry: "human",
     book: "Core Rulebook",
     description:
       "Humans are incredibly diverse. Some, such as half-elves and half-orcs, even have non-human ancestors.",
     abilityBoosts: "Two free ability boosts",
+    abilityFlaw: "—",
+  },
+  {
+    img: "Kashrishi.png",
+    ancestry: "kashrishi",
+    book: "Impossible Lands",
+    description:
+      "Kashrishi make their homes in remote areas of the world. These quiet beings have stout, durable frames and distinctive crystalline horns.",
+    abilityBoosts: "Constitution, free",
+    abilityFlaw: "—",
+  },
+  {
+    img: "Kitsune.png",
+    ancestry: "kitsune",
+    book: "Ancestry Guide",
+    description:
+      "Whether they pass unseen among other peoples or hold their tails high, kitsune are clever observers of the societies around them.",
+    abilityBoosts: "Charisma, free",
+    abilityFlaw: "—",
+  },
+  {
+    img: "Kobold.png",
+    ancestry: "kobold",
+    book: "Advanced Player's Guide",
+    description:
+      "Every kobold knows that their slight frame belies true, mighty draconic power.",
+    abilityBoosts: "Dexterity, Charisma, free",
+    abilityFlaw: "Constitution",
+  },
+  {
+    img: "Leshy.png",
+    ancestry: "leshy",
+    book: "Character Guide",
+    description:
+      "Guardians and emissaries of the environment, leshies are immortal spirits of nature temporarily granted a physical form.",
+    abilityBoosts: "Constitution, Wisdom, free",
+    abilityFlaw: "Intelligence",
+  },
+  {
+    img: "Lizardfolk.png",
+    ancestry: "lizardfolk",
+    book: "Character Guide",
+    description:
+      "Lizardfolk are consummate survivors, heirs to empires considered ancient even by the elves.",
+    abilityBoosts: "Strength, Wisdom, free",
+    abilityFlaw: "Intelligence",
+  },
+  {
+    img: "Nagaji.png",
+    ancestry: "nagaji",
+    book: "Impossible Lands",
+    description:
+      "With humanoid figures and serpentine heads, nagaji are heralds, companions, and servitors of powerful nagas.",
+    abilityBoosts: "Strength, free",
+    abilityFlaw: "—",
+  },
+  {
+    img: "Orc.png",
+    ancestry: "orc",
+    book: "Advanced Player's Guide",
+    description:
+      "Orcs are forged in the fires of violence and conflict, often from the moment they are born.",
+    abilityBoosts: "Strength, free",
+    abilityFlaw: "—",
+  },
+  {
+    img: "Poppet.png",
+    ancestry: "poppet",
+    book: "Grand Bazaar",
+    description:
+      "Poppets are small, basic constructs that typically help their owners with simple tasks.",
+    abilityBoosts: "Constitution, Charisma, free",
+    abilityFlaw: "Dexterity",
+  },
+  {
+    img: "Ratfolk.png",
+    ancestry: "ratfolk",
+    book: "Advanced Player's Guide",
+    description:
+      "Ysoki—as ratfolk call themselves—are a clever, adaptable, and fastidious ancestry who happily crowd their large families into the smallest of living spaces.",
+    abilityBoosts: "Dexterity, Intelligence, free",
+    abilityFlaw: "Strength",
+  },
+  {
+    img: "Shisk.png",
+    ancestry: "shisk",
+    book: "The Mwangi Expanse",
+    description:
+      "Shisks are secretive mountain-dwellers, bone-feathered humanoids who lurk underground in dark tunnels and caverns.",
+    abilityBoosts: "Intelligence, free",
+    abilityFlaw: "—",
+  },
+  {
+    img: "Shoony.png",
+    ancestry: "shoony",
+    book: "Pathfinder #153: Life's Long Shadows",
+    description:
+      "Diminutive humanoids who resemble squat, bipedal dogs, shoonies are sometimes mistaken for weak and insular pacifists.",
+    abilityBoosts: "Dexterity, Charisma, free",
+    abilityFlaw: "Constitution",
+  },
+  {
+    img: "Skeleton.png",
+    ancestry: "skeleton",
+    book: "Book of the Dead",
+    description:
+      "They're typically mindless creatures, lacking many of the abilities that make other undead a serious threat.",
+    abilityBoosts: "Dexterity, Charisma, free",
+    abilityFlaw: "Intelligence",
+  },
+  {
+    img: "Sprite.png",
+    ancestry: "sprite",
+    book: "Ancestry Guide",
+    description:
+      "Sprites are diminutive, whimsical, and exuberant creatures from the fey realm known as the First World.",
+    abilityBoosts: "Dexterity, Intelligence, free",
+    abilityFlaw: "Strength",
+  },
+  {
+    img: "Strix.png",
+    ancestry: "strix",
+    book: "Ancestry Guide",
+    description:
+      "Tengus are a gregarious and resourceful people, collecting and combining whatever innovations and traditions they happen across with those from their own long history.",
+    abilityBoosts: "Dexterity, free",
+    abilityFlaw: "—",
+  },
+  {
+    img: "Tengu.png",
+    ancestry: "tengu",
+    book: "Advanced Player's Guide",
+    description:
+      "Known as itarii in their own language, strix are reclusive avian humanoids devoted to their homelands and their tribes.",
+    abilityBoosts: "Dexterity, free",
+    abilityFlaw: "—",
+  },
+  {
+    img: "Vanara.png",
+    ancestry: "vanara",
+    book: "Impossible Lands",
+    description:
+      "Vanaras are inquisitive and mischievous monkey-like humanoids with short, soft fur, expressive eyes, and long, prehensile tails.",
+    abilityBoosts: "Dexterity, free",
+    abilityFlaw: "—",
+  },
+  {
+    img: "Vishkanya.png",
+    ancestry: "vishkanya",
+    book: "Impossible Lands",
+    description:
+      "Vishkanyas are ophidian humanoids who carry potent venom within their blood and saliva.",
+    abilityBoosts: "Dexterity, free",
     abilityFlaw: "—",
   },
 ];
