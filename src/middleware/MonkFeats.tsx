773edcdb679b3aa5c11c698a1b48a100758fd35b
Brenda Cayzac <brendacayzac@gmail.com>
2023-10-30 13:44:58 +0100

    added 10 monk feats

--- src/middleware/MonkFeats.tsx
+++ src/middleware/MonkFeats.tsx
@@ -34,4 +34,269 @@ export const monkFeats: Array<ClassFeat> = [
       </>
     ),
   },
+  {
+    id: 2,
+    name: "Dragon stance",
+    action: "one_action",
+    tags: ["monk", "stance"],
+    level: 1,
+    feat: "Feat 1",
+    description: (
+      <>
+        <p>
+          <i>Core Rulebook</i>
+        </p>
+        <p>
+          <span className="frequency">
+            <b>Requirements</b> You are unarmored.
+          </span>
+        </p>
+        <p>
+          You enter the stance of a dragon and make powerful leg strikes like a
+          lashing dragon’s tail. You can make dragon tail attacks that deal 1d10
+          bludgeoning damage. They are in the brawling group and have the
+          backswing, nonlethal, and unarmed traits.
+        </p>
+        <p>
+          While in Dragon Stance, you can ignore the first square of difficult
+          terrain while Striding.
+        </p>
+      </>
+    ),
+  },
+  {
+    id: 3,
+    name: "Gorilla stance",
+    action: "one_action",
+    tags: ["monk", "stance"],
+    level: 1,
+    feat: "Feat 1",
+    description: (
+      <>
+        <p>
+          <i>Advanced Player's Guide</i>
+        </p>
+        <p>
+          You lower yourself to the ground and take an imposing, knuckle-walking
+          stance. While in this stance, the only Strikes you can make are
+          gorilla slam unarmed attacks. These deal 1d8 bludgeoning damage; are
+          in the brawling group; and have the backswing, forceful, grapple,
+          nonlethal, and unarmed traits. While you are in Gorilla Stance, you
+          gain a +2 circumstance bonus to Athletics checks to Climb, and if you
+          roll a success on an Athletics check to Climb, you get a critical
+          success instead.
+        </p>
+      </>
+    ),
+  },
+  {
+    id: 4,
+    name: "Ki rush",
+    action: "",
+    tags: ["monk"],
+    level: 1,
+    feat: "Feat 1",
+    description: (
+      <>
+        <p>
+          <i>Core Rulebook</i>
+        </p>
+        <p>
+          You can use ki to move with extraordinary speed and make yourself
+          harder to hit. You gain the ki rush ki spell and a focus pool of 1
+          Focus Point. The rules for ki spells are summarized in the Key Terms
+          sidebar, and the full rules for focus spells appear in the Focus
+          Spells section.
+        </p>
+      </>
+    ),
+  },
+  {
+    id: 5,
+    name: "Ki strike",
+    action: "",
+    tags: ["monk"],
+    level: 1,
+    feat: "Feat 1",
+    description: (
+      <>
+        <p>
+          <i>Core Rulebook</i>
+        </p>
+        <p>
+          Your study of the flow of mystical energy allows you to harness it
+          into your physical strikes. You gain the ki strike ki spell and a
+          focus pool of 1 Focus Point. The rules for ki spells are summarized in
+          the Key Terms sidebar, and the full rules for focus spells appear in
+          the Focus Spells section.
+        </p>
+      </>
+    ),
+  },
+  {
+    id: 6,
+    name: "Monastic archer stance",
+    action: "one_action",
+    tags: ["monk", "stance"],
+    level: 1,
+    feat: "Feat 1",
+    description: (
+      <>
+        <p>
+          <i>Advanced Player's Guide</i>
+        </p>
+        <p>
+          <span className="frequency">
+            <b>Requirements</b> You are unarmored and wielding a longbow,
+            shortbow, or a bow with the monk trait.
+          </span>
+        </p>
+        <p>
+          You enter a specialized stance for a unique martial art centered
+          around the use of a bow. While in this stance, the only Strikes you
+          can make are those using longbows, shortbows, or bows with the monk
+          trait. You can use Flurry of Blows with these bows. You can use your
+          other monk feats or monk abilities that normally require unarmed
+          attacks with these bows when attacking within half the first range
+          increment (normally 50 feet for a longbow and 30 feet for a shortbow),
+          so long as the feat or ability doesn’t require a single, specific
+          Strike.
+        </p>
+        <p>
+          <b>Special</b> When you select this feat, you become trained in the
+          longbow, shortbow, and any simple and martial bows with the monk
+          trait. If you gain the expert strikes class feature, your proficiency
+          rank for these weapons increases to expert, and if you gain the master
+          strikes class feature, your proficiency rank for these weapons
+          increases to master.
+        </p>
+      </>
+    ),
+  },
+  {
+    id: 7,
+    name: "Monastic weaponry",
+    action: "",
+    tags: ["monk"],
+    level: 1,
+    feat: "Feat 1",
+    description: (
+      <>
+        <p>
+          <i>Core Rulebook</i>
+        </p>
+        <p>
+          You have trained with the traditional weaponry of your monastery or
+          school. You gain access to uncommon weapons that have the monk trait
+          and become trained in simple and martial monk weapons. When your
+          proficiency rank for unarmed attacks increases to expert or master,
+          your proficiency rank for these weapons increases to expert or master
+          as well.
+        </p>
+        <p>
+          You can use melee monk weapons with any of your monk feats or monk
+          abilities that normally require unarmed attacks, though not if the
+          feat or ability requires you to use a single specific type of attack,
+          such as Crane Stance.
+        </p>
+      </>
+    ),
+  },
+  {
+    id: 8,
+    name: "Mountain stance",
+    action: "one_action",
+    tags: ["monk", "stance"],
+    level: 1,
+    feat: "Feat 1",
+    description: (
+      <>
+        <p>
+          <i>Core Rulebook</i>
+        </p>
+        <p>
+          <span className="frequency">
+            <b>Requirements</b> You are unarmored and touching the ground.
+          </span>
+        </p>
+        <p>
+          You enter the stance of an implacable mountain—a technique created by
+          dwarven monks—allowing you to strike with the weight of an avalanche
+          and block blows with your garments. The only Strikes you can make are
+          falling stone unarmed attacks. These deal 1d8 bludgeoning damage; are
+          in the brawling group; and have the forceful, nonlethal, and unarmed
+          traits.
+        </p>
+        <p>
+          While in Mountain Stance, you gain a +4 item bonus to AC and a +2
+          circumstance bonus to any defenses against being Shoved or Tripped.
+          You have a Dexterity modifier cap to your AC of +0, meaning you don’t
+          add your Dexterity to your AC, and your Speeds are all reduced by 5
+          feet. The item bonus to AC from Mountain Stance is cumulative with
+          armor potency runes on your explorer’s clothing, mage armor, and
+          bracers of armor.
+        </p>
+      </>
+    ),
+  },
+  {
+    id: 9,
+    name: "Rain of embers stance",
+    action: "one_action",
+    tags: ["rare", "monk", "fire", "stance"],
+    level: 1,
+    feat: "Feat 1",
+    description: (
+      <>
+        <p>
+          <i>Core Rulebook</i>
+        </p>
+        <p>
+          <span className="frequency">
+            <b>Requirements</b> You are unarmored.
+          </span>
+        </p>
+        <p>
+          You enter the stance of an enraged phoenix, holding your fingers as
+          rigid as deadly talons while moving with quick, flickering gestures
+          that dance with biting flames. The only Strikes you can make are fire
+          talon Strikes. These deal 1d4 fire damage; are in the brawling group;
+          and have the agile, finesse, fire, nonlethal, and unarmed traits.
+        </p>
+        <p>
+          While in Rain of Embers Stance, you gain a +1 status bonus to AC and
+          fire resistance equal to half your level (minimum 1).
+        </p>
+      </>
+    ),
+  },
+   {
+    id: 10,
+    name: "Reflective ripple stance",
+    action: "one_action",
+    tags: ["monk", "evocation", "water", "stance"],
+    level: 1,
+    feat: "Feat 1",
+    description: (
+      <>
+        <p>
+          <i>Core Rulebook</i>
+        </p>
+        <p>
+          <span className="frequency">
+            <b>Requirements</b> You are unarmored.
+          </span>
+        </p>
+        <p>
+         You enter a stance of fluid grace as small amounts of water flow with your movements and attacks. You can make flowing wave attacks that deal 1d6 bludgeoning damage. They are in the brawling group and have the agile, disarm, finesse, nonlethal, trip, unarmed, and water traits.
+        </p>
+        <p>
+         While in Reflective Ripple Stance, you gain a +1 circumstance bonus to Athletics checks to Disarm, Swim, or Trip, and you gain a +2 circumstance bonus to your Reflex DC to avoid being Disarmed and Tripped.
+        </p>
+        <p>
+            <b>Special</b> This feat gains your choice of either the divine or occult trait, matching your ki spell tradition if possible.
+        </p>
+      </>
+    ),
+  },
 ];
