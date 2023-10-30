8875be5525b8118dd2daebcd37ef2c8677810917
Brenda Cayzac <brendacayzac@gmail.com>
2023-10-30 13:59:18 +0100

    added 10 more monk feats

--- src/middleware/MonkFeats.tsx
+++ src/middleware/MonkFeats.tsx
@@ -270,7 +270,7 @@ export const monkFeats: Array<ClassFeat> = [
       </>
     ),
   },
-   {
+  {
     id: 10,
     name: "Reflective ripple stance",
     action: "one_action",
@@ -288,13 +288,201 @@ export const monkFeats: Array<ClassFeat> = [
           </span>
         </p>
         <p>
-         You enter a stance of fluid grace as small amounts of water flow with your movements and attacks. You can make flowing wave attacks that deal 1d6 bludgeoning damage. They are in the brawling group and have the agile, disarm, finesse, nonlethal, trip, unarmed, and water traits.
+          You enter a stance of fluid grace as small amounts of water flow with
+          your movements and attacks. You can make flowing wave attacks that
+          deal 1d6 bludgeoning damage. They are in the brawling group and have
+          the agile, disarm, finesse, nonlethal, trip, unarmed, and water
+          traits.
         </p>
         <p>
-         While in Reflective Ripple Stance, you gain a +1 circumstance bonus to Athletics checks to Disarm, Swim, or Trip, and you gain a +2 circumstance bonus to your Reflex DC to avoid being Disarmed and Tripped.
+          While in Reflective Ripple Stance, you gain a +1 circumstance bonus to
+          Athletics checks to Disarm, Swim, or Trip, and you gain a +2
+          circumstance bonus to your Reflex DC to avoid being Disarmed and
+          Tripped.
         </p>
         <p>
-            <b>Special</b> This feat gains your choice of either the divine or occult trait, matching your ki spell tradition if possible.
+          <b>Special</b> This feat gains your choice of either the divine or
+          occult trait, matching your ki spell tradition if possible.
+        </p>
+      </>
+    ),
+  },
+  {
+    id: 11,
+    name: "Stoked flame stance",
+    action: "one_action",
+    tags: ["monk", "evocation", "fire", "stance"],
+    level: 1,
+    feat: "Feat 1",
+    description: (
+      <>
+        <p>
+          <i>Secrets of Magic</i>
+        </p>
+        <p>
+          <span className="frequency">
+            <b>Requirements</b> You are unarmored.
+          </span>
+        </p>
+        <p>
+          You enter a stance of fast, fiery movements. You can make flashing
+          spark attacks that deal 1d8 slashing damage. They are in the brawling
+          group and have the forceful, nonlethal, sweep, and unarmed traits. If
+          you have access to the flashing sparks’ critical specialization
+          effect, you can take an alternate effect instead: if your critical
+          Strike dealt damage, the target takes 1d6 persistent fire damage.
+        </p>
+        <p>
+          While in Stoked Flame Stance, you gain a +5-foot status bonus to your
+          Speed. If you have incredible movement, increase the benefit from
+          incredible movement to a +15-foot status bonus plus 5 feet for every 4
+          levels beyond 3rd.
+        </p>
+        <p>
+          <b>Special</b> This feat gains your choice of either the divine or
+          occult trait, matching your ki spell tradition if possible.
+        </p>
+      </>
+    ),
+  },
+  {
+    id: 12,
+    name: "Stumbling stance",
+    action: "one_action",
+    tags: ["monk", "stance"],
+    level: 1,
+    feat: "Feat 1",
+    description: (
+      <>
+        <p>
+          <i>Advanced player's Guide</i>
+        </p>
+        <p>
+          <span className="frequency">
+            <b>Prerequisites</b> trained in Deception.
+          </span>
+        </p>
+        <p>
+          You enter a seemingly unfocused stance that mimics the movements of
+          the inebriated—bobbing, weaving, leaving false openings, and
+          distracting your enemies from your true movements. While in this
+          stance, you gain a +1 circumstance bonus to Deception checks to Feint.
+          The only Strikes you can make are stumbling swing unarmed attacks.
+          These deal 1d8 bludgeoning damage; are in the brawling group; and have
+          the agile, backstabber, finesse, nonlethal, and unarmed traits. If an
+          enemy hits you with a melee Strike while in this stance, it becomes
+          flat-footed against the next stumbling swing Strike you make against
+          it before the end of your next turn.
+        </p>
+      </>
+    ),
+  },
+  {
+    id: 13,
+    name: "Tiger stance",
+    action: "one_action",
+    tags: ["monk", "stance"],
+    level: 1,
+    feat: "Feat 1",
+    description: (
+      <>
+        <p>
+          <i>Core Rulebook</i>
+        </p>
+        <p>
+          <span className="frequency">
+            <b>Requirements</b> You are unarmored.
+          </span>
+        </p>
+        <p>
+          You enter the stance of a tiger and can make tiger claw attacks. These
+          deal 1d8 slashing damage; are in the brawling group; and have the
+          agile, finesse, nonlethal, and unarmed traits. On a critical success
+          with your tiger claws, if you deal damage, the target takes 1d4
+          persistent bleed damage.
+        </p>
+        <p>
+          As long as your Speed is at least 20 feet while in Tiger Stance, you
+          can Step 10 feet.
+        </p>
+      </>
+    ),
+  },
+  {
+    id: 14,
+    name: "Wolf stance",
+    action: "one_action",
+    tags: ["monk", "stance"],
+    level: 1,
+    feat: "Feat 1",
+    description: (
+      <>
+        <p>
+          <i>Core Rulebook</i>
+        </p>
+        <p>
+          <span className="frequency">
+            <b>Requirements</b> You are unarmored.
+          </span>
+        </p>
+        <p>
+          You enter the stance of a wolf, low to the ground with your hands held
+          like fanged teeth. You can make wolf jaw unarmed attacks. These deal
+          1d8 piercing damage; are in the brawling group; and have the agile,
+          backstabber, finesse, nonlethal, and unarmed traits.
+        </p>
+        <p>
+          If you’re flanking a target while in Wolf Stance, your wolf jaw
+          unarmed attacks also gain the trip trait.
+        </p>
+      </>
+    ),
+  },
+  {
+    id: 15,
+    name: "Ancestral weaponry",
+    action: "",
+    tags: ["monk"],
+    level: 2,
+    feat: "Feat 2",
+    description: (
+      <>
+        <p>
+          <i>Advanced Player's Guide</i>
+        </p>
+        <p>
+          <span className="frequency">
+            <b>Prerequisites</b> Monastic Weaponry, a feat granting access to
+            all weapons with an ancestry trait (such as elf or orc).
+          </span>
+        </p>
+        <p>
+          You blend monastic techniques with the ancestral fighting styles of
+          your people. Choose an ancestry for which you have access to all
+          weapons with that trait. For you, melee weapons with that ancestry
+          trait and either the agile or finesse trait gain the monk trait.
+        </p>
+      </>
+    ),
+  },
+  {
+    id: 16,
+    name: "Brawling focus",
+    action: "",
+    tags: ["monk"],
+    level: 2,
+    feat: "Feat 2",
+    description: (
+      <>
+        <p>
+          <i>Advanced Player's Guide</i>
+        </p>
+        <p>
+          You know how to make the most of your attacks when fighting
+          hand-to-hand. You gain access to the critical specialization effects
+          of unarmed strikes in the brawling group and weapons in the brawling
+          group. If you have Monastic Weaponry, you also gain the critical
+          specialization effects of all monk weapons in which you are trained.
         </p>
       </>
     ),
