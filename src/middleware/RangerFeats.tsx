f82ed8ac303e71c6ab4a823d6f37bdde207bfb3f
Brenda Cayzac <brendacayzac@gmail.com>
2023-11-20 13:09:28 +0100

    added 10 more ranger feats

--- src/middleware/RangerFeats.tsx
+++ src/middleware/RangerFeats.tsx
@@ -780,4 +780,285 @@ export const rangerFeats: Array<ClassFeat> = [
       </>
     ),
   },
+  {
+    id: 31,
+    name: "Scout's warning",
+    action: "",
+    tags: ["ranger"],
+    level: 4,
+    feat: "Feat 4",
+    description: (
+      <>
+        <p>
+          <i>Core Rulebook</i>
+        </p>
+        <p>
+          <span className="frequency">
+            <b>Trigger</b> You are about to roll a Perception or Survival check
+            for initiative.
+          </span>
+        </p>
+        <p>
+          You visually or audibly warn your allies of danger, granting them each
+          a +1 circumstance bonus to their initiative rolls. Depending on
+          whether you use gestures or call out, this action gains either the
+          visual or the auditory trait, respectively.
+        </p>
+      </>
+    ),
+  },
+  {
+    id: 32,
+    name: "Snare specialist",
+    action: "",
+    tags: ["ranger"],
+    level: 4,
+    feat: "Feat 4",
+    description: (
+      <>
+        <p>
+          <i>Core Rulebook</i>
+        </p>
+        <p>
+          <span className="frequency">
+            <b>Prerequisites</b> expert in Crafting, Snare Crafting.
+          </span>
+        </p>
+        <p>
+          You specialize in creating quick traps to obstruct your enemies on the
+          battlefield. If your proficiency rank in Crafting is expert, you gain
+          the formulas for three common or uncommon snares. If your rank is
+          master, you gain 6. If your rank is legendary, you gain 9.
+        </p>
+        <p>
+          Each day during your daily preparations, you can prepare four snares
+          from your formula book for quick deployment; if they normally take 1
+          minute to Craft, you can Craft them with 3 Interact actions. The
+          number of snares increases to six if you have master proficiency in
+          Crafting and eight if you have legendary proficiency in Crafting.
+          Snares prepared in this way don’t cost you any resources to Craft.
+        </p>
+      </>
+    ),
+  },
+  {
+    id: 33,
+    name: "Soothing mist",
+    action: "",
+    tags: ["ranger"],
+    level: 4,
+    feat: "Feat 4",
+    description: (
+      <>
+        <p>
+          <i>Advanced Player's Guide</i>
+        </p>
+        <p>
+          <span className="frequency">
+            <b>Prerequisites</b> warden spells.
+          </span>
+        </p>
+        <p>
+          You have a connection to the healing properties of nature and can
+          produce a magical mist to heal damage and stop burning or bleeding.
+          You gain the soothing mist warden spell. Increase the number of Focus
+          Points in your focus pool by 1.
+        </p>
+      </>
+    ),
+  },
+  {
+    id: 34,
+    name: "Twin Parry",
+    action: "one_action",
+    tags: ["ranger"],
+    level: 4,
+    feat: "Feat 4",
+    description: (
+      <>
+        <p>
+          <i>Core Rulebook</i>
+        </p>
+        <p>
+          <span className="frequency">
+            <b>Requirements</b> You are wielding two melee weapons, one in each
+            hand.
+          </span>
+        </p>
+        <p>
+          You can use two weapons to deflect attacks. You gain a +1 circumstance
+          bonus to AC until the start of your next turn, or a +2 circumstance
+          bonus if either weapon has the parry trait. You lose this circumstance
+          bonus if you no longer meet this feat’s requirement.
+        </p>
+      </>
+    ),
+  },
+  {
+    id: 35,
+    name: "Wolf in sheep's clothing",
+    action: "",
+    tags: ["ranger"],
+    level: 4,
+    feat: "Feat 4",
+    description: (
+      <>
+        <p>
+          <i>Dark Archive</i>
+        </p>
+        <p>
+          <span className="frequency">
+            <b>Prerequisites</b> trained in Deception.
+          </span>
+        </p>
+        <p>
+          With a bit of hide, some antlers, and some cast-off fur, you can
+          create a disguise that will fool even an experienced hunter. You can
+          Impersonate creatures with the animal, beast, or plant traits, so long
+          as they are either the same size or one size larger than you and have
+          a body shape that conforms at least vaguely to your own (in other
+          words, a typical humanoid could Impersonate a bear or an arboreal, but
+          not a horse, giant scorpion, or assassin vine). Impersonating a larger
+          creature doesn’t change your actual size.
+        </p>
+      </>
+    ),
+  },
+  {
+    id: 36,
+    name: "Additional recollection",
+    action: "",
+    tags: ["ranger"],
+    level: 6,
+    feat: "Feat 6",
+    description: (
+      <>
+        <p>
+          <i>Advanced Player's Guide</i>
+        </p>
+        <p>
+          <span className="frequency">
+            <b>Trigger</b> You succeed or critically succeed at a check to
+            Recall Knowledge on your hunted prey.
+          </span>
+        </p>
+        <p>
+          You scan the battlefield quickly, remembering critical details about
+          multiple opponents you face. You immediately attempt a check to Recall
+          Knowledge about a different creature you can perceive.
+        </p>
+      </>
+    ),
+  },
+  {
+    id: 37,
+    name: "Defy fey",
+    action: "",
+    tags: ["rare", "ranger", "gunslinger", "investigator"],
+    level: 6,
+    feat: "Feat 6",
+    description: (
+      <>
+        <p>
+          <i>Lost Omens: Impossible Lands</i>
+        </p>
+        <p>
+          <span className="frequency">
+            <b>Prerequisites</b> Greenwatch Initiate.
+          </span>
+        </p>
+        <p>
+          You are keen to the wiles of the fey and can usually shake off their
+          magic. Whenever you fail, but don’t critically fail, a save against a
+          spell with a duration of at least 1 round cast by a fey creature, you
+          can attempt a second save against the original DC at the start of your
+          next turn to end any lingering effects the spell might have. This can
+          end persistent damage caused by a spell but can’t reverse any effects
+          that have been resolved already (such as damage dealt when the spell
+          was cast).
+        </p>
+      </>
+    ),
+  },
+  {
+    id: 38,
+    name: "Ephemeral tracking",
+    action: "",
+    tags: ["ranger"],
+    level: 6,
+    feat: "Feat 6",
+    description: (
+      <>
+        <p>
+          <i>Advanced Player's Guide</i>
+        </p>
+        <p>
+          <span className="frequency">
+            <b>Prerequisites</b> expert in Survival, warden spells.
+          </span>
+        </p>
+        <p>
+          You are able to track your quarry through impossible places. You gain
+          the ephemeral tracking warden spell. Increase the number of Focus
+          Points in your focus pool by 1.
+        </p>
+      </>
+    ),
+  },
+  {
+    id: 39,
+    name: "Fey tracker",
+    action: "",
+    tags: ["rare", "ranger", "gunslinger", "investigator"],
+    level: 6,
+    feat: "Feat 6",
+    description: (
+      <>
+        <p>
+          <i>Lost Omens: Impossible Lands</i>
+        </p>
+        <p>
+          <span className="frequency">
+            <b>Prerequisites</b> Greenwatch Initiate.
+          </span>
+        </p>
+        <p>
+          You are exceptionally skilled at noticing the subtle techniques fey
+          use to avoid notice. You gain a +2 circumstance bonus to Survival
+          checks to Track fey creatures, to Perception checks to Seek for hidden
+          fey, and to your Perception DC to resist a fey creature’s attempt to
+          Create a Diversion.
+        </p>
+      </>
+    ),
+  },
+  {
+    id: 40,
+    name: "Fey tracker",
+    action: "",
+    tags: ["ranger"],
+    level: 6,
+    feat: "Feat 6",
+    description: (
+      <>
+        <p>
+          <i>Player Core</i>
+        </p>
+        <p>
+          <span className="frequency">
+            <b>Prerequisites</b> Initiate Warden.
+          </span>
+        </p>
+        <p>
+          Your mastery of warden magic increases. You gain your choice of one
+          warden spell from the master warden spells (or another you have access
+          to).
+        </p>
+        <p>
+          <b>Special</b> You can take this feat multiple times, choosing a
+          different master warden spell each time.
+        </p>
+      </>
+    ),
+  },
 ];
